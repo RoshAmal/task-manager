a524bbfbb3fe42e1d3d500b3d86a70ae2c442420
RoshAmal <amal.rosh707@gmail.com>
2024-09-12 14:28:23 +0530

    Created cards for each task with dummy data

---
+++ frontend/src/components/TaskList/TaskList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './TaskList.css';
+
+const TaskList = () => {
+  const tasks = [
+    { index: 1, taskItem: 'Clean room' },
+    { index: 2, taskItem: 'Go to Gym' },
+    { index: 3, taskItem: 'Cook breakfast' }
+  ];
+
+  return (
+    <div>
+      <h4 className="task-list-heading">Task List</h4>
+      <div>
+        {tasks.map(task => (
+          <div key={task.index} className="task-card">
+            <h5>{task.taskItem}</h5>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
