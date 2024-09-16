7fdb202a632448f965c387e6ef54b504f97c8a17
RoshAmal <amal.rosh707@gmail.com>
2024-09-16 16:00:22 +0530

    Removed log statements

--- frontend/src/components/CreateTaskPane/CreateTaskPane.js
+++ frontend/src/components/CreateTaskPane/CreateTaskPane.js
@@ -4,18 +4,15 @@ import axios from 'axios';
 const CreateTaskPane = () => {
 	const [newTask,setNewTask] = useState('');
 	const handleInputChange = (e) => {
-		console.log('Input changed:', e.target.value);
 		setNewTask(e.target.value);
 	};
 	const handleCreateTask = async () => {
-		console.log('New task:',newTask);
 		if (newTask.trim()==='') return;
 		try{
 			const response = await axios.post('http://localhost:5000/api/tasks', {
 				taskItem: newTask,
 			});
 			if (response.status === 201) {
-				console.log('Task Created:', response.data.task);
 				setNewTask('');
 			}
 		} catch(error){
