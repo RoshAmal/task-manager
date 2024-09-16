bd2f4a174e98da70e251ba8e984c885b0f47023d
RoshAmal <amalrosh707@gmail.com>
2024-09-16 15:06:14 +0530

    API integration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
+
+const taskSchema = new mongoose.Schema({
+	taskItem: { type: String, required: true }
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+app.use(cors());
 
 mongoose.connect(
 	'mongodb://localhost:27017/taskmanager',{
@@ -13,6 +22,17 @@ mongoose.connect(
 app.use(express.json());
 app.get('/',(req,res)=>{
 	res.send('Task Manager API is running');
+});
+
+app.post('/api/tasks', async (req,res) => {
+	try {
+		console.log("reached");
+		const newTask = new Task({ taskItem: req.body.taskItem });
+		await newTask.save();
+		res.status(201).json({ message: 'Task created successfully', task: newTask });
+	} catch (error) {
+		res.status(500).json({ message: 'Error creating task', error });
+	}
 });
 
 const PORT = process.env.PORT || 5000;
