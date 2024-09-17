708f6bfb5351c6cce3646a80ea9ffdfd7918f1aa
RoshAmal <amalrosh707@gmail.com>
2024-09-17 22:14:07 +0530

    Task details modal can be closed

--- frontend/src/components/TaskDetails/TaskDetails.js
+++ frontend/src/components/TaskDetails/TaskDetails.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TaskDetails.css';
 
 const TaskDetails = ({ task }) => {
-  return (
-    <div className="task-details-overlay">
-      <div className="task-details-modal">
-        <h3>Task Details</h3>
-        <p><strong>Task:</strong> {task.taskItem}</p>
-      </div>
-    </div>
-  );
+
+    const [isVisible,setIsVisible] = useState(true);
+
+    const handleClose = () => {
+        setIsVisible(false)
+    };
+    if (!isVisible) return null;
+
+    return (
+        <div className="task-details-overlay">
+        <div className="task-details-modal">
+            <h3>Task Details</h3>
+            <p><strong>Task:</strong> {task.taskItem}</p>
+            <button className="close-btn" onClick={handleClose}>x</button>
+        </div>
+        </div>
+    );
 };
 
 export default TaskDetails;
