fa8173f160bafe44a345bd3d834d6fe9d5166f89
RoshAmal <amalrosh707@gmail.com>
2024-09-17 22:32:44 +0530

    Updating close logc

--- frontend/src/components/TaskDetails/TaskDetails.js
+++ frontend/src/components/TaskDetails/TaskDetails.js
@@ -3,9 +3,20 @@ import './TaskDetails.css';
 
 const TaskDetails = ({ task, onClose }) => {
 
+    const handleOverlayClick = (e) => {
+        onClose();
+    };
+    const handleModalClick = (e) => {
+        e.stopPropagation();
+    };
+
     return (
-        <div className="task-details-overlay">
-        <div className="task-details-modal">
+        <div 
+            className="task-details-overlay"
+            onClick={handleOverlayClick}>
+        <div 
+            className="task-details-modal"
+            onClick={handleModalClick}>
             <h3>Task Details</h3>
             <p><strong>Task:</strong> {task.taskItem}</p>
             <button className="close-btn" onClick={onClose}>x</button>
