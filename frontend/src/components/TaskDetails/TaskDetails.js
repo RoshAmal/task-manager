79b2c585d6b50c212939a865d623fc48617941c4
RoshAmal <amal.rosh707@gmail.com>
2024-09-23 21:58:07 +0530

    Delete button

--- frontend/src/components/TaskDetails/TaskDetails.js
+++ frontend/src/components/TaskDetails/TaskDetails.js
@@ -4,6 +4,7 @@ import './TaskDetails.css';
 const TaskDetails = ({ task, onClose, onSave }) => {
 
     const [editedTaskItem,setEditedTaskItem] = useState(task.taskItem);
+    const [dropdownVisibile,setDropdownVisible] = useState(false);
 
     const handleModalClick = (e) => {
         e.stopPropagation();
@@ -13,6 +14,13 @@ const TaskDetails = ({ task, onClose, onSave }) => {
         setEditedTaskItem(e.target.value);
     };
 
+    const handleDropdownVisible = () => {
+        setDropdownVisible(!dropdownVisibile);
+    };
+
+    const handleDelete = () => {
+
+    };
     const handleCloseClick = () => {
         onSave({...task,taskItem:editedTaskItem});
         onClose();
@@ -25,7 +33,22 @@ const TaskDetails = ({ task, onClose, onSave }) => {
         <div 
             className="task-details-modal"
             onClick={handleModalClick}>
-            <button className="close-btn" onClick={handleCloseClick}>x</button>
+            <div className="modal-buttons">
+                <div className="dropdown-wrapper">
+                    <button className="ellipsis" onClick={handleDropdownVisible}>
+                    &#8943;
+                    </button>
+                    {dropdownVisibile && (
+                        <div className="dropdown-menu">
+                            <div className="dropdown-option" onClick={handleDelete}>
+                                Delete Task
+                            </div>
+                        </div>
+                        )
+                    }
+                </div>
+                <button className="close-btn" onClick={handleCloseClick}>x</button>
+            </div>
             <h3>Task Details</h3>      
             <p>
                 <strong>Task: </strong> 
